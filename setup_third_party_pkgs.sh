23a4c2199ae50f5bbac823bcb996f1924d82ca76
Elshamy <moelshamy742@gmail.com>
2025-10-03 21:45:54 +0400

    Add a gazebo version detector in the script file and create a gz sim .repo file

--- setup_third_party_pkgs.sh
+++ setup_third_party_pkgs.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 set -e
 
-# Create ThirdParty.repos for the current ROS2 distributions
-if [ "$ROS_DISTRO" = "humble" ]; then
-  cp ThirdParty/ThirdParty.humble.repos ThirdParty/ThirdParty.repos
+# Detect Gazebo type
+if command -v gz >/dev/null 2>&1 && gz help sim >/dev/null 2>&1; then
+    GZ_TYPE="sim"
+elif command -v gazebo >/dev/null 2>&1; then
+  GZ_TYPE="classic"
 else
+  GZ_TYPE="classic"
+fi
+
+echo "Detected ROS_DISTRO=$ROS_DISTRO, Gazebo=$GZ_TYPE"
+
+# Create ThirdParty.repos for the current ROS2 distribution
+if [ "$ROS_DISTRO" = "humble" ]; then
+  if [ "$GZ_TYPE" = "sim" ]; then
+    echo "Using Humble + Gazebo Sim repos"
+    cp ThirdParty/ThirdParty.humble.gz_sim.repos ThirdParty/ThirdParty.repos
+  elif [ "$GZ_TYPE" = "classic" ]; then
+    echo "Using Humble + Gazebo Classic repos"
+    cp ThirdParty/ThirdParty.humble.gazebo_classic.repos ThirdParty/ThirdParty.repos
+  else
+    cp ThirdParty/ThirdParty.humble.gazebo_classic.repos ThirdParty/ThirdParty.repos
+  fi
+else
+  echo "👉 Using template repos for $ROS_DISTRO"
   sed "s/\$ROS_DISTRO/$ROS_DISTRO/g" ThirdParty/ThirdParty.template.repos > ThirdParty/ThirdParty.repos
 fi
 
