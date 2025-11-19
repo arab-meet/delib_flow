b983b4866cb793b8068344496663fa283fa32ff0
ElSayed ElSheikh <elsayed.elsheikh97@gmail.com>
2025-11-19 16:27:41 +0200

    make flake8 happy

--- tiago_sim/launch/yolo_tiago.launch.py
+++ tiago_sim/launch/yolo_tiago.launch.py
@@ -3,14 +3,13 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-
     tiago_gazebo_dir = get_package_share_directory('tiago_sim')
 
     # YOLOv11
