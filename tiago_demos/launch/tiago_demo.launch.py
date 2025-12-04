b8681e9c1267d942c44bb1d7c5e846363210108f
ElSayed ElSheikh <elsayed.elsheikh97@gmail.com>
2025-12-04 20:06:56 +0200

    Add demos boilerplate

---
+++ tiago_demos/launch/tiago_demo.launch.py
@@ -0,0 +1,31 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    # Paths to configuration files
+    behavior_tree_xml = os.path.join(
+        get_package_share_directory('tiago_demos'), 'trees', 'navigation_demo.xml'
+    )
+    locations_cfg = os.path.join(
+        get_package_share_directory('tiago_demos'), 'config', 'our_map.yaml'
+    )
+
+    return LaunchDescription(
+        [
+            Node(
+                package='tiago_demos',
+                executable='btcpp_engine',
+                name='btcpp_engine',
+                output='screen',
+                parameters=[
+                    {'use_sim_time': True},
+                    {'behavior_tree': behavior_tree_xml},
+                    {'target_locations': locations_cfg},
+                ],
+            )
+        ]
+    )
