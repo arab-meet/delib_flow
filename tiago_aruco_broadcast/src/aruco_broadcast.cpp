e4d5645ffba3ebdc265f5932983f7a2fa304efad
Mohamed <mohamedfathalla9018@gmail.com>
2025-12-05 14:03:46 +0000

    updated

--- tiago_aruco_broadcast/src/aruco_broadcast.cpp
+++ tiago_aruco_broadcast/src/aruco_broadcast.cpp
@@ -27,19 +27,7 @@ class camera : public rclcpp::Node
    }
 
    private:
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
-
->>>>>>> 2efb961 (updated aruco)
-=======
-
->>>>>>> 4ccead3 (updated)
-=======
-   
->>>>>>> 2efb961 (updated aruco)
    void camera_callback( const ros2_aruco_interfaces::msg::ArucoMarkers &aruco )
    {
 
