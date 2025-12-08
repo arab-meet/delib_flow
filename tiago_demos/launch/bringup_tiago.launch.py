12f747dab63d85fddb0007a27b279c6902e6042c
ElSayed ElSheikh <elsayed.elsheikh97@gmail.com>
2025-12-08 17:08:08 +0200

    Add picking demo

---
+++ tiago_demos/launch/bringup_tiago.launch.py
@@ -0,0 +1,55 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    sim_launch = IncludeLaunchDescription(
+        PathJoinSubstitution(
+            [
+                FindPackageShare('tiago_sim'),
+                'launch',
+                'tiago_sim.launch.py',
+            ]
+        )
+    )
+
+    nav2_launch = IncludeLaunchDescription(
+        PathJoinSubstitution(
+            [
+                FindPackageShare('tiago_nav'),
+                'launch',
+                'tiago_nav2.launch.py',
+            ]
+        )
+    )
+
+    moveit2_launch = IncludeLaunchDescription(
+        PathJoinSubstitution(
+            [
+                FindPackageShare('tiago_grab'),
+                'launch',
+                'tiago_moveit2.launch.py',
+            ]
+        )
+    )
+
+    grab2_bt_grabber_launch = IncludeLaunchDescription(
+        PathJoinSubstitution(
+            [
+                FindPackageShare('tiago_grab'),
+                'launch',
+                'grab2_reach_object.launch.py',
+            ]
+        )
+    )
+
+    return LaunchDescription(
+        [
+            sim_launch,
+            nav2_launch,
+            moveit2_launch,
+            grab2_bt_grabber_launch,
+        ]
+    )
