bd9bb3231f88592ed1c2f1b1d2b1ab74a1087ab0
ElSayed ElSheikh <elsayed.elsheikh97@gmail.com>
2025-12-18 19:48:17 +0200

    Add picking demo (#43)
    - Add `grab2_bt_grabber` behavior tree execution server to perform pick object action
    - Add pick object xml demo
    - Clean up configuration files and replace with one YAML file `tiago_positions` only to load pre-defined trajectories for different joints
    - Add bringup launch file to launch the whole system 
    - Add relevant documentation

--- tiago_demos/launch/tiago_demo.launch.py
+++ tiago_demos/launch/tiago_demo.launch.py
@@ -1,31 +1,58 @@
-import os
-
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    # Paths to configuration files
-    behavior_tree_xml = os.path.join(
-        get_package_share_directory('tiago_demos'), 'trees', 'our_map_example.xml'
+    behavior_tree_declaration = DeclareLaunchArgument(
+        'tree',
+        default_value='our_map_example.xml',
+        description='Behavior Tree XML file to execute, Check trees directory for examples',
     )
-    locations_cfg = os.path.join(
-        get_package_share_directory('tiago_demos'), 'config', 'our_map.yaml'
+
+    # TODO: Implement loading predefined locations from a YAML file
+    locations_declaration = DeclareLaunchArgument(
+        'locations',
+        default_value='our_map.yaml',
+        description='YAML file with predefined target locations to load into the blackboard',
+    )
+
+    # Paths to configuration files
+    behavior_tree_xml = PathJoinSubstitution(
+        [
+            FindPackageShare('tiago_demos'),
+            'trees',
+            [LaunchConfiguration('tree')],
+        ]
+    )
+    locations_cfg = PathJoinSubstitution(
+        [
+            FindPackageShare('tiago_demos'),
+            'config',
+            [LaunchConfiguration('locations')],
+        ]
+    )
+
+    bt_executor_node = Node(
+        package='tiago_demos',
+        executable='btcpp_engine',
+        name='bt_executor',
+        output='screen',
+        parameters=[
+            {
+                'use_sim_time': True,
+                'behavior_tree': behavior_tree_xml,
+                'target_locations': locations_cfg,
+            }
+        ],
     )
 
     return LaunchDescription(
         [
-            Node(
-                package='tiago_demos',
-                executable='btcpp_engine',
-                name='btcpp_engine',
-                output='screen',
-                parameters=[
-                    {'use_sim_time': True},
-                    {'behavior_tree': behavior_tree_xml},
-                    {'target_locations': locations_cfg},
-                ],
-            )
+            behavior_tree_declaration,
+            locations_declaration,
+            bt_executor_node,
         ]
     )
