4dad14192c918b0e614ee83373764808500b0832
sherif1152 <sfathey606@gmail.com>
2025-12-22 11:32:53 +0200

    refactor(launch): reorganize imports and add comments for clarity in tiago_behavior.launch.py

--- tiago_behavior/launch/tiago_behavior.launch.py
+++ tiago_behavior/launch/tiago_behavior.launch.py
@@ -3,23 +3,21 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 
+from launch_ros.actions import Node
+
 
 def generate_launch_description():
-    # Get package directory
     tiago_behavior_dir = get_package_share_directory('tiago_behavior')
 
-    # Declare tree.xml path argument
     tree_xml_arg = DeclareLaunchArgument(
         'tree_xml_path',
         default_value=os.path.join(tiago_behavior_dir, 'config', 'tree.xml'),
         description='Full path to the behavior tree XML file',
     )
 
-    # Create the behavior tree node
     bt_navigator = Node(
         package='tiago_behavior',
         executable='main_bt',
